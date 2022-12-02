daa06ef7fb906e4486bcfafd3c9e2bea18cc627f
Manoj <manoj.singh@nivesh.com>
2022-12-02 16:27:34 +0530

    second library

---
+++ TestLibTwo/src/main/java/com/nivesh/production/testlibtwo/MainActivityLib.kt
@@ -0,0 +1,11 @@
+package com.nivesh.production.testlibtwo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityLib : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_lib)
+    }
+}
